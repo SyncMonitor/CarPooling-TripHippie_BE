830eeaa3d3c03cf9dcba5f408f9c8e967c79e0af
lcala99 <40360727+lcala99@users.noreply.github.com>
2023-07-19 17:31:33 +0200

    inizialize spring project

---
+++ src/main/java/com/TripHippie/CarPoolingTripHippie_BE/CarPoolingTripHippieBeApplication.java
@@ -0,0 +1,13 @@
+package com.TripHippie.CarPoolingTripHippie_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarPoolingTripHippieBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarPoolingTripHippieBeApplication.class, args);
+	}
+
+}
